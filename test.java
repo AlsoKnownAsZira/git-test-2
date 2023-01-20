c5bba3a431d80a5bf52a3922876800dafd8b7719
brilsat <brilyansatria10@gmail.com>
2023-01-21 02:37:36 +0700

    revert

--- test.java
+++ test.java
@@ -1,7 +1,5 @@
  public class test{
     public static void main(String[] args) {
         System.out.println("TESTING PART 2");
-        System.out.println("TAMBAHAN123");
-        System.out.println(123);
     }
  }
